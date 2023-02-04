73a80e6a1d6cbf056195cc8ea32bcd2fd5a11283
Jozark <joshua.meyer.personal@gmail.com>
2023-02-04 17:02:22 +0100

    add loading spinner

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,35 +2,36 @@ import { useState } from "react";
 import styles from "./App.module.css";
 import Button from "./components/Button/button";
 import Image from "./components/Image/image";
+import LoadingSpinner from "./components/LoadingSpinner/loadingSpinner";
 
 function App() {
-  const [imageUrl, setImageUrl] = useState(
+  const [imageurl, setImageurl] = useState(
     "https://media.discordapp.net/attachments/913148795632631838/1071435446627864607/DT1502_cropped2.png"
   );
   const [isLoading, setIsLoading] = useState(false);
 
   const handleButtonClick = async () => {
     setIsLoading(true);
-    const response = await fetch("http://localhost:3000/api/getImage", {
+    const response = await fetch("http://localhost:3001/api/getImage", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        imageUrl,
+        imageurl,
       }),
     });
 
     const data = await response.json();
-    setIsLoading(false);
-    setImageUrl(data);
     console.log(data);
+    setImageurl(data.image_url);
+    setIsLoading(false);
   };
 
   return (
     <div className={styles.app}>
       <div className={styles.container}>
-        {isLoading ? <>test</> : <Image source={imageUrl} alt=""></Image>}
+        {isLoading ? <LoadingSpinner /> : <Image source={imageurl} alt="" />}
         <Button type="rectangle" onButtonClick={handleButtonClick}>
           Reimagine
         </Button>
