36ce5535df0c1521673285e3d47c091d622183ca
Jozark <joshua.meyer.personal@gmail.com>
2023-02-07 00:09:10 +0100

    add tx fee for variation

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,8 +4,14 @@ import {
   Metaplex,
   Nft,
   NftWithToken,
+  toPublicKey,
 } from "@metaplex-foundation/js";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { AnchorProvider } from "@project-serum/anchor";
+import { Wallet } from "@project-serum/anchor/dist/cjs/provider";
+import {
+  WalletAdapterNetwork,
+  WalletNotConnectedError,
+} from "@solana/wallet-adapter-base";
 import {
   ConnectionProvider,
   useWallet,
@@ -17,11 +23,19 @@ import {
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
+import {
+  clusterApiUrl,
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
 import assert from "assert";
 import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import Button from "./components/Button/button";
+import HistoryGrid from "./components/HistoryGrid/HistoryGrid";
 import Image from "./components/Image/image";
 import ImageGrid from "./components/ImageGrid/imageGrid";
 import LoadingSpinner from "./components/LoadingSpinner/loadingSpinner";
@@ -30,7 +44,6 @@ import {
   requestVariationAndMetadataUpdate,
 } from "./services/images.services";
 import { MetaDataOrNft, NftWithUrl } from "./types";
-import HistoryGrid from "./components/HistoryGrid/HistoryGrid";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 // ========================================================================================================
@@ -44,9 +57,7 @@ const NETWORK_TYPE = WalletAdapterNetwork.Devnet;
 const wallets = [new PhantomWalletAdapter()];
 
 // set up connection to chain
-const endpoint = clusterApiUrl(NETWORK_TYPE);
-const connection = new Connection(endpoint, "confirmed");
-const metaplex = new Metaplex(connection);
+const clusterEndpoint = clusterApiUrl(NETWORK_TYPE);
 
 function App() {
   const wallet = useWallet();
@@ -66,12 +77,68 @@ function App() {
     }
   }, [wallet]);
 
+  // Reload on page refresh // TODO this is mainly for development, not sure if we need this in PROD
+  if (REFRESH_STATE_ON_PAGE_RELOAD) {
+    useEffect(() => {
+      fetchCollectionAndSetSelected();
+    }, []);
+  }
+  // Reload once the wallet is connected (or disconnected, but we exit early in that case)
+  useEffect(() => {
+    fetchCollectionAndSetSelected();
+  }, [walletPub]);
+
+  const getProvider = (): AnchorProvider => {
+    const connection = new Connection(clusterEndpoint, "processed");
+    const provider = new AnchorProvider(connection, wallet as Wallet, {
+      preflightCommitment: "processed",
+    });
+    return provider;
+  };
+
+  const getMetaplexConnection = (): Metaplex => {
+    const connection = new Connection(clusterEndpoint, "confirmed");
+    const metaplex = new Metaplex(connection);
+    return metaplex;
+  };
+
+  const sendTransaction = async (solAmount: number): Promise<boolean> => {
+    try {
+      const connection = new Connection(clusterEndpoint, "processed");
+      const provider = getProvider();
+
+      if (!provider.wallet.publicKey) throw new WalletNotConnectedError();
+
+      const txn = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: provider.wallet.publicKey,
+          toPubkey: toPublicKey("9RKaq2srT5eewGo7DkGVXfkmr1eBser2VGLa37b65JKN"),
+          lamports: solAmount * LAMPORTS_PER_SOL,
+        })
+      );
+
+      const signature = await wallet.sendTransaction(txn, connection);
+
+      const latestBlockHash = await connection.getLatestBlockhash();
+
+      await connection.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: signature,
+      });
+      return true;
+    } catch (e) {
+      console.log("transaction failed", e);
+      return false;
+    }
+  };
   /// Return all nfts from our collection if any.
   ///
   /// Expects [walletPub] to be present
   const getCollectionNfts = async (): Promise<Array<MetaDataOrNft>> => {
     assert(walletPub);
 
+    const metaplex = getMetaplexConnection();
     const allNfts: FindNftsByOwnerOutput = await metaplex
       .nfts()
       .findAllByOwner({ owner: walletPub });
@@ -83,7 +150,7 @@ function App() {
 
   /// If we have a wallet connected, get NFTs from our collection and select first
   const fetchCollectionAndSetSelected = async (
-    nftWithUrl: NftWithUrl | null
+    nftWithUrl?: NftWithUrl | null
   ) => {
     if (!walletPub) return;
 
@@ -105,6 +172,7 @@ function App() {
         );
       setCollectionNFTs(collectionNftsWithImages);
 
+      // TODO refactor. m monkey brain can not follow
       let _selectedNft;
       if (nftWithUrl) {
         _selectedNft = nftWithUrl.nft;
@@ -124,6 +192,7 @@ function App() {
         setSelectedNFT({ nft: _selectedNft, url });
       }
 
+      const metaplex = getMetaplexConnection();
       const metdata = await metaplex.nfts().findByMint({
         mintAddress: new PublicKey(
           (_selectedNft as Metadata).mintAddress.toString()
@@ -134,22 +203,19 @@ function App() {
     }
   };
 
-  // Reload on page refresh // TODO this is mainly for development, not sure if we need this in PROD
-  if (REFRESH_STATE_ON_PAGE_RELOAD) {
-    useEffect(() => {
-      fetchCollectionAndSetSelected(null);
-    }, []);
-  }
-  // Reload once the wallet is connected (or disconnected, but we exit early in that case)
-  useEffect(() => {
-    fetchCollectionAndSetSelected(null);
-  }, [walletPub]);
-
   // TODO refactor again, we only need the evolution for the reset param
   const handleAlterImageClick = async (endpoint: string, evolution: number) => {
-    if (!selectedNFT?.url) return;
+    if (!selectedNFT?.url || !walletPub) return;
 
     setIsLoading(true);
+
+    const transactionSucceded = await sendTransaction(0.1);
+
+    if (!transactionSucceded) {
+      console.log("nice try bro 💀");
+      setIsLoading(false);
+      return;
+    }
 
     const mintAddress = new PublicKey(
       (selectedNFT?.nft as Metadata).mintAddress.toString()
@@ -161,10 +227,13 @@ function App() {
       evolution
     );
 
-    if (!success) return; // TODO maybe show a small error snackbar?
+    if (!success) {
+      setIsLoading(false);
+      return;
+    } // TODO maybe show a small error snackbar?
 
     // TODO not sure if we need this
-    await fetchCollectionAndSetSelected(null);
+    await fetchCollectionAndSetSelected();
     setIsLoading(false);
   };
 
@@ -236,7 +305,7 @@ function App() {
 }
 
 const AppWithProvider = () => (
-  <ConnectionProvider endpoint={endpoint}>
+  <ConnectionProvider endpoint={clusterEndpoint}>
     <WalletProvider wallets={wallets} autoConnect>
       <WalletModalProvider>
         <App />
